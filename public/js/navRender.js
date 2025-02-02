be99c9612e5c9bc75f618ba7687746bd28985997
dan <dan@dancreative.co>
2025-02-01 19:17:43 -0600

    lots of changes

---
+++ public/js/navRender.js
@@ -0,0 +1,13 @@
+
+document.addEventListener("DOMContentLoaded", function(){
+    fetch("navbar.html")
+    .then(response => response.text())
+    .then(data =>{
+        document.getElementById("nav-placeholder").innerHTML = data;
+    })
+    .catch(err =>{
+        console.log(err);
+    })
+        
+    
+})
