e0dca9c045eec83a5ad0d2ad9985505e551ed688
dan <dan@dancreative.co>
2025-02-06 22:46:34 -0600

    Added menu highlight mobile

--- public/js/navActions.js
+++ public/js/navActions.js
@@ -4,7 +4,26 @@ const toggleMenu = () =>{
         
     }
     else{
-        document.getElementById("menu").style.display = "block";
+        const menuDiv = document.getElementById("menu");
+        menuDiv.style.display = "block";
         document.body.style.overflow = "hidden";
+        
+
+        const currentDoc = document.title;
+        const menuItems =  menuDiv.childNodes;
+        console.log(menuItems)
+
+        menuItems.forEach(item =>{
+            console.log(item.textContent + " " + currentDoc)
+            if(item.textContent == currentDoc){
+                console.log(item)
+                item.classList.add('active');
+            }
+
+        });
     }
+    
 }
+
+
+    
