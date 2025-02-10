5ea6ced7ba1057dd504dcd4f1ad4540cf7187343
dan <dan@dancreative.co>
2025-02-09 22:51:18 -0600

    Nav done

--- public/js/navActions.js
+++ public/js/navActions.js
@@ -1,10 +1,17 @@
     const menuBtn = document.getElementById("menu-button");
     const menu = document.getElementById("popup-menu");
     const exitBtn = document.getElementById("exit-btn");
+    const listItems = document.getElementById("nav-list").children;
 
     menuBtn.onclick = function(){
         menu.style.display = "block";
     }
     exitBtn.onclick = function(){
         menu.style.display = "none";
+    }
+
+    window.onclick = function(event){
+        if(event.target === menu || event.target.tagName === "li"){
+            menu.style.display = "none";
+        }
     }
