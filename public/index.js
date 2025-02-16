e0fecd96b079a85879cbf6840bfcdaccad20e0f6
dan <dan@dancreative.co>
2025-02-15 21:51:19 -0600

    Fixed stuff. Scroll is next >:|

---
+++ public/index.js
@@ -0,0 +1,8 @@
+const popup = document.getElementById("popup-card-article");
+const mainContainer = document.getElementsByClassName('main-container');
+const openNews = () =>{
+    popup.style.display = "block"; 
+}
+const closeArticle = () =>{
+    popup.style.display = "none";
+}
